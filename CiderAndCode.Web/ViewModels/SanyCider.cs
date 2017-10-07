86e81d8b0823862d5305250f87986f0fbd773acc
Sanyyouisf <sanymamdoh@yahoo.com>
2017-10-07 09:22:13 -0700

    adding cider

---
+++ CiderAndCode.Web/ViewModels/SanyCider.cs
@@ -0,0 +1,18 @@
+﻿using CiderAndCode.Web.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CiderAndCode.Web.ViewModels
+{
+    public class SanyCider
+    {
+        public int Id { get; set; }
+        public string AppleType { get; set; }
+        public string Notes { get; set; }
+        public int HowManyGallons { get; set; }
+        public bool filtered { get; set; }
+        public string Bushel { get; set; }
+    }
+}
